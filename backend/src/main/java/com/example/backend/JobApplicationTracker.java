ea80c87b6aed2f700ea78fddbd9ff88b44c74dc7
WorkWithSoham <sohamtembe98@gmail.com>
2023-06-03 20:44:24 -0500

    Spring framework installed

--- backend/src/main/java/com/example/backend/JobApplicationTracker.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class JobApplicationTracker extends Application {
-
-}
